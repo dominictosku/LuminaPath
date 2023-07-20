5c3fb3e8359e7efa21a635af3fc4c81b5b28549e
xXAinzXx <dominic.tosku@gmail.com>
2023-07-20 12:23:04 +0200

    feat: create user

--- LuminaPath-Backend/LuminaPath/Controllers/LuminaUserController.cs
+++ LuminaPath-Backend/LuminaPath/Controllers/LuminaUserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Data.Classes;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,45 @@ namespace LuminaPath.Controllers
 		public LuminaUserController(UserManager<IdentityUser> userManager)
 		{
 			_userManager = userManager;
+		}
+
+		[HttpGet("{username}")]
+		public async Task<ActionResult<LuminaUser>> GetUser(string username)
+		{
+			IdentityUser? user = await _userManager.FindByNameAsync(username);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			return new LuminaUser
+			{
+				UserName = user.UserName ?? "Error",
+				Email = user.Email ?? "Error"
+			};
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<LuminaUser>> PostUser(LuminaUser user)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var result = await _userManager.CreateAsync(
+				new IdentityUser() { UserName = user.UserName, Email = user.Email },
+				user.Password
+			);
+
+			if (!result.Succeeded)
+			{
+				return BadRequest(result.Errors);
+			}
+
+			user.Password = null;
+			return Created("", user);
 		}
 	}
 }
