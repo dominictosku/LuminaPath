284e750aba8aa055e73abba7def217f10f368581
xXAinzXx <dominic.tosku@gmail.com>
2023-07-21 12:47:49 +0200

    feat: added migration for LuminaUser relation

--- LuminaPath-Backend/Data/Models/Base/Media.cs
+++ LuminaPath-Backend/Data/Models/Base/Media.cs
@@ -14,11 +14,15 @@ namespace Data.Models.Base
     public abstract class Media : IBasicInfo
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(50)]
         [Display(Name = "Title")]
         public string Name { get; set; }
         public string? Description { get; set; }
         public string? Genre { get; set; }
+
+		[Display(Name = "Release Date")]
+		public DateTime? ReleaseDate { get; set; }
     }
 }
