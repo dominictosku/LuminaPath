da699b05622c64f0dc451aab99cc9ca9ae3adc59
xXAinzXx <dominic.tosku@gmail.com>
2023-07-22 17:46:52 +0200

    style: datepicker

--- LuminaPath-Frontend/components/Events/VDatePicker.vue
+++ LuminaPath-Frontend/components/Events/VDatePicker.vue
@@ -1,28 +1,26 @@
 <template>
   <DatePicker v-model="date">
     <template #default="{ togglePopover }">
-      <button
-        class="px-3 py-2 bg-blue-500 text-sm text-white font-semibold rounded-md"
-        @click="togglePopover"
-      >
-        Select date
-      </button>
+        <button type="button" class="px-3 py-2 bg-blue-500 text-sm text-white font-semibold rounded-md"
+          @click="togglePopover">
+          Select date
+        </button>
     </template>
   </DatePicker>
 </template>
   
-  <script>
-  import { DatePicker } from 'v-calendar';
-  import 'v-calendar/style.css';
-  
-  export default {
-    components: {
-      DatePicker
-    },
-    data() {
-      return {
-        date: new Date(),
-      };
-    },
-  }
-  </script>
+<script>
+import { DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
+
+export default {
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+}
+</script>
