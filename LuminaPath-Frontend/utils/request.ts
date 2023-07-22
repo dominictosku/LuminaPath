323041fd007230b618e8903b471dc22474e45518
xXAinzXx <dominic.tosku@gmail.com>
2023-07-22 16:50:57 +0200

    feat: redirect to login

--- LuminaPath-Frontend/utils/request.ts
+++ LuminaPath-Frontend/utils/request.ts
@@ -3,32 +3,39 @@ import { IGame } from './games'
 import { type Credentials } from "~/utils/user";
 import { useUseStore } from "~/stores/user";
 
-const runtimeConfig = useRuntimeConfig()
-
-const url = runtimeConfig.public.API_ENDPOINT + "/api"
-const userStore = useUseStore()
-
 export async function fetchGames(): Promise<Array<IGame>> {
+    const runtimeConfig = useRuntimeConfig()
+    const url = runtimeConfig.public.API_ENDPOINT + "/api"
     const result: IGame[] = await $fetch<IGame[]>(url + "/games")
     return result
 }
 
 export async function deleteGame(id: number){
+    const userStore = useUseStore()
+    const runtimeConfig = useRuntimeConfig()
+    const url = runtimeConfig.public.API_ENDPOINT + "/api"
     await axios.delete(url + "/games?id=" + id, userStore.config)
     return
 }
 
 export async function PostGame(game: IGame){
+    const userStore = useUseStore()
+    const runtimeConfig = useRuntimeConfig()
+    const url = runtimeConfig.public.API_ENDPOINT + "/api"
     console.log(userStore.config)
     await axios.post(url + "/games", game, userStore.config)
     return
 }
 
 export async function LoginUser(Credentials: Credentials){
+    const runtimeConfig = useRuntimeConfig()
+    const url = runtimeConfig.public.API_ENDPOINT + "/api"
     const { data } = await axios.post(url + "/LuminaUser/BearerToken", Credentials)
     return data.token
 }
 
 export async function CreateUser(Credentials: Credentials){
+    const runtimeConfig = useRuntimeConfig()
+    const url = runtimeConfig.public.API_ENDPOINT + "/api"
     await axios.post(url + "/LuminaUser", Credentials)
 }
