5133ced65efb24d89f40580986730b47572803a1
xXAinzXx <dominic.tosku@gmail.com>
2023-07-22 19:05:27 +0200

    fix: missing css todo

--- LuminaPath-Frontend/pages/index/Planer.vue
+++ LuminaPath-Frontend/pages/index/Planer.vue
@@ -6,43 +6,20 @@ import { Calendar } from 'v-calendar';
     <ion-content :fullscreen="true">
       <EventsVWeek />
       <ion-list>
-        <ion-item-sliding>
-          <ion-item>
-            <ion-label>Sliding Item with End Options</ion-label>
-          </ion-item>
-
-          <ion-item-options>
-            <ion-item-option>Favorite</ion-item-option>
-            <ion-item-option color="danger">Delete</ion-item-option>
-          </ion-item-options>
-        </ion-item-sliding>
-
-        <ion-item-sliding>
+        <ion-item-sliding v-for="item in 10">
           <ion-item-options side="start">
-            <ion-item-option color="success">Archive</ion-item-option>
+            <ion-item-option color="success">Completed</ion-item-option>
           </ion-item-options>
 
           <ion-item>
-            <ion-label>Sliding Item with Start Options</ion-label>
-          </ion-item>
-        </ion-item-sliding>
-
-        <ion-item-sliding>
-          <ion-item-options side="start">
-            <ion-item-option color="success">Archive</ion-item-option>
-          </ion-item-options>
-
-          <ion-item>
-            <ion-label>Sliding Item with Options on Both Sides</ion-label>
+            <ion-label>Todo: idk</ion-label>
           </ion-item>
 
           <ion-item-options side="end">
-            <ion-item-option>Favorite</ion-item-option>
             <ion-item-option color="danger">Delete</ion-item-option>
           </ion-item-options>
         </ion-item-sliding>
       </ion-list>
-      <Drag />
       <EventsVFullCalendar class="sm:p-12" />
     </ion-content>
   </ion-page>
