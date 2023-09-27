90179271f46d965d961f5613a231923f268a6c1f
xXAinzXx <dominic.tosku@gmail.com>
2023-09-27 20:29:47 +0200

    refactor: store

--- LuminaPath-Frontend/utils/classes/baseStore.ts
+++ LuminaPath-Frontend/utils/classes/baseStore.ts
@@ -4,62 +4,58 @@ export function BaseStore<T extends IBasicInfo>(prefix: string) {
   const PageIndex: Ref<number> = ref(1);
   const TotalPages: Ref<number> = ref(1);
   const Prefix: string = prefix;
-
   const Media = computed(() => {
     return MediaList.value;
   });
 
-  async function getMedia(mediaFilter?: MediaFilter): Promise<T[]> {
-    let response = await fetchMedia<T>(Prefix);
-    if (typeof response === "object" && response != null)
-      MediaList.value = response;
-    return response;
-  }
+  const api = {
+    getMedia: async (mediaFilter?: MediaFilter): Promise<T[]> => {
+      let response = await fetchMedia<T>(Prefix);
+      if (typeof response === "object" && response != null)
+        MediaList.value = response;
+      return response;
+    },
 
-  async function getMediaAll(count?: number): Promise<T[]> {
-    let response = await fetchMediaAll<T>(count ?? 100, Prefix);
-    if (typeof response === "object" && response != null)
-      MediaList.value = response;
-    return response;
-  }
+    getMediaAll: async (count?: number): Promise<T[]> => {
+      let response = await fetchMediaAll<T>(count ?? 100, Prefix);
+      if (typeof response === "object" && response != null)
+        MediaList.value = response;
+      return response;
+    },
 
-  async function getPaginatedMedia(mediaFilter: MediaFilter): Promise<T[]> {
-    let response = await fetchPaginatedMedia<T>(Prefix, mediaFilter );
-    if (typeof response === "object" && response != null)
-      MediaList.value = response.data;
-    PageIndex.value = response.currentPage;
-    TotalPages.value = response.pages;
-    return response.data;
-  }
+    getPaginatedMedia: async (mediaFilter: MediaFilter): Promise<T[]> => {
+      let response = await fetchPaginatedMedia<T>(Prefix, mediaFilter);
+      if (typeof response === "object" && response != null)
+        MediaList.value = response.data;
+      PageIndex.value = response.currentPage;
+      TotalPages.value = response.pages;
+      return response.data;
+    },
 
-  async function getMediaById(id: number): Promise<T | undefined> {
-    return await fetchMediaById<T>(id, Prefix);
-  }
+    getMediaById: async (id: number): Promise<T | undefined> => {
+      return await fetchMediaById<T>(id, Prefix);
+    },
 
-  async function removeMedia(id: number) {
-    await deleteMedia(id, Prefix);
-    await getMedia();
-    return;
-  }
+    removeMedia: async (id: number) => {
+      await deleteMedia(id, Prefix);
+      await api.getMedia();
+      return;
+    },
 
-  async function createMedia(media: T) {
-    if (media.id == 0) {
-      await PostMedia(media, Prefix);
-    } else {
-      await PutMedia(media, Prefix);
-    }
-    await getMedia();
-    return;
-  }
+    createMedia: async (media: T) => {
+      if (media.id == 0) {
+        await PostMedia(media, Prefix);
+      } else {
+        await PutMedia(media, Prefix);
+      }
+      await api.getMedia();
+      return;
+    },
+  };
   return {
     Media,
     PageIndex,
     TotalPages,
-    getMedia,
-    getMediaAll,
-    getPaginatedMedia,
-    getMediaById,
-    createMedia,
-    removeMedia,
+    api,
   };
 }
