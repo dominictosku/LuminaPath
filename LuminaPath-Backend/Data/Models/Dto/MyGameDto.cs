c03d1b1b08debb40b68382d0a6d38cc1d128839e
xXAinzXx <dominic.tosku@gmail.com>
2023-10-02 22:16:06 +0200

    feat: only one game to select fo mygame

--- LuminaPath-Backend/Data/Models/Dto/MyGameDto.cs
+++ LuminaPath-Backend/Data/Models/Dto/MyGameDto.cs
@@ -1,14 +1,23 @@
 ﻿using Data.Models.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Models.Dto
 {
-	public class MyGameDto : MyMedia
+	public class MyGameDto
 	{
+		public int Id { get; set; }
+		[Range(1, 10)]
+		public byte? Rating { get; set; }
+		public DateTime? StartDate { get; set; }
+		public DateTime? EndDate { get; set; }
+		public ProgressionStatus Status { get; set; }
+		public int? TimeSpend { get; set; }
+		public int GameId { get; set; }
 		public GamesNoIncludeDto? Game { get; set; }
 	}
 }
