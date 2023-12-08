59493ea488af1952760f56a3e953f46557ec2038
xXAinzXx <dominic.tosku@gmail.com>
2023-12-08 16:09:26 +0100

    feat: image service

---
+++ LuminaPath-Backend/Data/Migrations/20231208144409_MediaFilesAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MediaFilesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Games",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Games");
+        }
+    }
+}
---
+++ LuminaPath-Backend/Data/Services/ImageHub.cs
@@ -0,0 +1,56 @@
+using Data.Interfaces;
+using Data.Models.Dto.Blob;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services
+{
+    public class ImageHub
+    {
+        private readonly IAzureStorage _azureStorage;
+        public ImageHub(IAzureStorage azureStorage) 
+        {
+            _azureStorage = azureStorage;
+        }
+
+        public async Task<BlobResponseDto> GetImage(string fileName)
+        {
+            var image = await _azureStorage.DownloadAsync(fileName);
+            if(image == null)
+            {
+                return new BlobResponseDto()
+                {
+                    Error = true,
+                    Status = "Could no find image"
+                };
+            }
+            var result = new BlobResponseDto()
+            {
+                Error = false,
+                Status = "success",
+                Blob = image
+            };
+
+            return result; 
+        }
+
+        public async Task<BlobResponseDto> SaveImage(IFormFile file)
+        {
+            if(file == null)
+            {
+                return new BlobResponseDto()
+                {
+                    Status = "File was null",
+                    Error = true
+                };
+            }
+            string fileName = new Guid().ToString();
+            var result = await _azureStorage.UploadAsync(file);
+            return result;
+        }
+    }
+}
