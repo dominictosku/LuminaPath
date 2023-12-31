05c18b69525852353b0ffa54e5c338b25cc00403
xXAinzXx <dominic.tosku@gmail.com>
2023-12-31 14:22:04 +0100

    chore: improved seperation of concerns

--- src/Server/DependencyInjection.cs
+++ src/Server/DependencyInjection.cs
@@ -22,81 +22,104 @@ namespace Server
 	public static class DependencyInjection
 	{
 		public const string MyAllowSpecificOrigins = "SPAConfig";
-		public static IServiceCollection AddServer(this IServiceCollection services)
+		public static IServiceCollection AddServer(this IServiceCollection services, IConfiguration config)
 		{
-			services.AddControllers(options =>
-			{
-				options.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider());
-			});
+            AddApi(services);
+            AddServices(services, config);
+			AddCors(services);
 
-			services.AddCors(o => o.AddPolicy(MyAllowSpecificOrigins, builder =>
-			{
-				builder.WithOrigins("http://localhost:3000")
-					   .WithOrigins("http://127.0.0.1:3000")
-					   .AllowAnyMethod()
-					   .AllowAnyHeader()
-					   .AllowCredentials();
-			}));
-
-			return services;
-		}
-
-		public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration config)
-		{
-			string connectionString = Environment.GetEnvironmentVariable("AZURE_CONNECTIONSTRING")
-				?? config.GetSection("Azure")["BlobConnectionString"]
-				?? throw new Exception("No blob connectionfound");
-			string containerName = config.GetSection("Azure")["BlobContainerName"]
-				?? throw new Exception("No blob container name found");
-
-			services.AddEndpointsApiExplorer();
-			services.AddSwaggerGen();
-
-			services.AddScoped<ITokenGenerator, JwtService>();
-			services.AddTransient<IGenericRepo<Game>, GenericRepo<Game>>();
-			services.AddTransient<IGenericRepo<MyGame>, GenericRepo<MyGame>>();
-			services.AddTransient<IGenericRepo<GamesQuest>, GenericRepo<GamesQuest>>();
-			services.AddScoped<IAzureStorage, AzureStorage>(s =>
-				new AzureStorage(connectionString, containerName, s.GetRequiredService<ILogger<AzureStorage>>()));
-			services.AddTransient<GameRepo>();
-			services.AddTransient<MyGameRepo>();
-
-			return services;
+            return services;
 		}
 
 		public static async Task ConfigureServer(this WebApplication app)
 		{
-			app.UseForwardedHeaders(new ForwardedHeadersOptions
-			{
-				ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-			});
-
 			await ConfigureEnvironment(app);
 
-			app.Use(async (context, next) =>
-			{
-				await next();
+			AddMiddleware(app);
 
-				if (context.Response.StatusCode == (int)HttpStatusCode.Unauthorized)
-				{
-					await context.Response.WriteAsync("Session expired, please login");
-				}
-
-				if (context.Response.StatusCode == (int)HttpStatusCode.Forbidden)
-				{
-					await context.Response.WriteAsync("You have not permission to access this");
-				}
-			});
-
-			app.UseCors(MyAllowSpecificOrigins);
+            app.UseCors(MyAllowSpecificOrigins);
 			app.UseHttpsRedirection();
 			app.MapControllers();
 		}
 
+        private static void AddApi(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddControllers(options =>
+            {
+                options.ModelMetadataDetailsProviders.Add(new SystemTextJsonValidationMetadataProvider());
+            });
+        }
+
+        private static void AddServices(IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenGenerator, JwtService>();
+            AddStorageService(services, config);
+            AddRepositories(services);
+        }
+
+        private static void AddStorageService(IServiceCollection services, IConfiguration config)
+		{
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_CONNECTIONSTRING")
+				?? config.GetSection("Azure")["BlobConnectionString"]
+				?? throw new Exception("No blob connectionfound");
+
+            string containerName = config.GetSection("Azure")["BlobContainerName"]
+                ?? throw new Exception("No blob container name found");
+
+            services.AddScoped<IAzureStorage, AzureStorage>(s =>
+				new AzureStorage(connectionString, containerName, s.GetRequiredService<ILogger<AzureStorage>>()));
+        }
+
+		private static void AddRepositories(IServiceCollection services)
+		{
+            services.AddTransient<IGenericRepo<Game>, GenericRepo<Game>>();
+            services.AddTransient<IGenericRepo<MyGame>, GenericRepo<MyGame>>();
+            services.AddTransient<IGenericRepo<GamesQuest>, GenericRepo<GamesQuest>>();
+            services.AddTransient<GameRepo>();
+            services.AddTransient<MyGameRepo>();
+        }
+
+		private static void AddCors(IServiceCollection services)
+		{
+            services.AddCors(o => o.AddPolicy(MyAllowSpecificOrigins, builder =>
+            {
+                builder.WithOrigins("http://localhost:3000")
+                       .WithOrigins("http://127.0.0.1:3000")
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials();
+            }));
+        }
+
+		private static void AddMiddleware(WebApplication app)
+		{
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == (int)HttpStatusCode.Unauthorized)
+                {
+                    await context.Response.WriteAsync("Session expired, please login");
+                }
+
+                if (context.Response.StatusCode == (int)HttpStatusCode.Forbidden)
+                {
+                    await context.Response.WriteAsync("You have not permission to access this");
+                }
+            });
+        }
+
 		private static async Task ConfigureEnvironment(WebApplication app)
 		{
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
 				app.UseSwaggerUI();
