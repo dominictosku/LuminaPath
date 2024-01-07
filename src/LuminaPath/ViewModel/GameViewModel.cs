792c966ae050ddb24220a014f5c922044fc7e87e
xXAinzXx <dominic.tosku@gmail.com>
2024-01-07 18:58:41 +0100

    feat: upload image

---
+++ src/LuminaPath/ViewModel/GameViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Core.Models.Gaming;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace LuminaPath.ViewModel
+{
+    public class GameViewModel : Game
+    {
+        public Stream? File { get; set; }
+    }
+}
