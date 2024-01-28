fa1d1b1368ecdc07ca610f1d22286a9d6ef4b5e6
xXAinzXx <dominic.tosku@gmail.com>
2024-01-28 16:11:37 +0100

    refactor: generic controller

--- src/Application/Controllers/MyGamesController.cs
+++ src/Application/Controllers/MyGamesController.cs
@@ -10,22 +10,56 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 namespace Application.Controllers
 {
-	[Route("api/[controller]")]
+    [Route("api/[controller]")]
 	[ApiController]
 	[Authorize]
-	public class MyGamesController : MyMediaController<MyGame, MyGameDto>
+	public class MyGamesController : GenericController<MyGame, MyGameDto>
 	{
+		private readonly IUnitOfWork unitOfWork;
 		private readonly UserManager<LuminaUser> _userManager;
 		private readonly ILogger<GamesController> _logger;
-		private readonly IUnitOfWork unitOfWork;
 
 		public MyGamesController(IUnitOfWork unitOfWork, UserManager<LuminaUser> userManager,
-			ILogger<GamesController> logger, IMapper mapper) : base(unitOfWork.MyGameRepo, mapper, userManager)
+			ILogger<GamesController> logger, IMapper mapper) : base(unitOfWork.MyGameRepo, mapper)
 		{
 			this.unitOfWork = unitOfWork;
 			_userManager = userManager;
 			_logger = logger;
 			Includes = new List<string> { "Game" };
+		}
+
+		[HttpPost]
+		public override async Task<ActionResult<MyGameDto>> PostAsync(MyGame viewModel)
+		{
+			var (user, userId) = await GetUserAndUserIdAsync(_userManager);
+			if (user == null)
+				return NotFound("User not found, please login");
+			if (IsMediaAlreadyAdded(viewModel.MediaId, viewModel.Id, userId))
+			{
+				return BadRequest("This is game already added");
+			}
+			viewModel.LuminaUserId = user.Id;
+			return await base.PostAsync(viewModel);
+		}
+
+		[HttpPut("{id}")]
+		public override async Task<IActionResult> PutAsync(int id, MyGame viewModel)
+		{
+			var (user, userId) = await GetUserAndUserIdAsync(_userManager);
+			if (user == null)
+				return NotFound("User not found, please login");
+			if (IsMediaAlreadyAdded(viewModel.MediaId, viewModel.Id, userId))
+			{
+				return BadRequest("This is game already added");
+			}
+			viewModel.LuminaUserId = user.Id;
+			return await base.PutAsync(id, viewModel);
+		}
+
+		protected bool IsMediaAlreadyAdded(int id, int myId, string userId)
+		{
+			var entities = _service.GetAllNoTrack();
+			return entities.Any(e => e.MediaId == id && e.Id != myId && e.LuminaUserId == userId);
 		}
 	}
 }
