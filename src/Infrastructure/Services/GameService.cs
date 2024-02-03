1d93995a1897f39e8e978093ec1644cf203524b9
xXAinzXx <dominic.tosku@gmail.com>
2024-02-03 17:41:03 +0100

    refactor: controller logic to services

--- src/Infrastructure/Services/GameService.cs
+++ src/Infrastructure/Services/GameService.cs
@@ -1,9 +1,14 @@
-﻿using AutoMapper;
+﻿using Application.Helper;
+using AutoMapper;
+using Domain.Dto.Gaming;
+using Domain.Entities;
 using Domain.Models.Gaming;
 using Infrastructure.Interfaces.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +16,39 @@ namespace Infrastructure.Services
 {
     public class GameService : GenericModelService<Game>
     {
-        public GameService(IGameRepository repo, IMapper mapper) : base(repo, mapper) { }
-    }
+		protected new readonly IGameRepository _repository;
+		public GameService(IGameRepository repo, IMapper mapper) : base(repo, mapper) 
+		{
+			_repository = repo;
+		}
+
+		public async Task<Result<PaginatedResult<TDto>, FailedResult>> GetAndMapEntities<TDto>(MediaFilter mediaFilter, IEnumerable<string> includes, string? userId)
+		{
+			Expression<Func<Game, bool>> filter = GetFilterExpression(mediaFilter, userId);
+
+			PaginatedList<Game> entities = userId != null
+				? await _repository.GetAllPaginated(mediaFilter.Paging, userId, filter)
+				: await _repository.GetAllPaginated(mediaFilter.Paging, filter, includes: includes);
+
+			var entitiesDto = Mapper.Map<IEnumerable<Game>, IEnumerable<TDto>>(entities);
+			return new PaginatedResult<TDto>(entitiesDto, entities.PageIndex, entities.TotalPages);
+		}
+
+		private Expression<Func<Game, bool>> GetFilterExpression(MediaFilter mediaFilter, string? userId)
+		{
+			Expression<Func<Game, bool>> filter = g => true;
+
+			if (mediaFilter.SearchString != null)
+			{
+				filter = g => g.Name.Contains(mediaFilter.SearchString);
+			}
+
+			if (mediaFilter.MyMedia && userId != null)
+			{
+				filter = filter.And(g => g.MyGames != null && g.MyGames.Any(m => m.LuminaUserId == userId));
+			}
+
+			return filter;
+		}
+	}
 }
--- src/LuminaPath/Controllers/GamesController.cs
+++ src/LuminaPath/Controllers/GamesController.cs
@@ -17,12 +17,12 @@ namespace LuminaPath.Controllers
 {
 	public class GamesController : GenericController<Game, GamesDto>
 	{
-        protected readonly IGameRepository _repository;
+		private readonly new GameService _service;
         private readonly ILogger<GamesController> _logger;
 
 		public GamesController(IGameRepository gameRepo, GameService service, ILogger<GamesController> logger, IMapper mapper) : base(service, mapper)
 		{
-			_repository = gameRepo;
+			_service = service;
 			_logger = logger;
             Includes = new List<string>() { "Image" };
 		}
@@ -32,31 +32,10 @@ namespace LuminaPath.Controllers
 		public override async Task<ActionResult<PaginatedResult<GamesDto>>> Get([FromQuery] MediaFilter mediaFilter)
 		{
 			string? userId = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-			Expression<Func<Game, bool>> filter = GetFilterExpression(mediaFilter, userId);
-
-			PaginatedList<Game> entities = userId != null
-				? await _repository.GetAllPaginated(mediaFilter.Paging, userId, filter)
-				: await _repository.GetAllPaginated(mediaFilter.Paging, filter, includes: Includes);
-
-			var entitiesDto = Mapper.Map<IEnumerable<Game>, IEnumerable<GamesDto>>(entities);
-			return new PaginatedResult<GamesDto>(entitiesDto, entities.PageIndex, entities.TotalPages);
-		}
-
-		private Expression<Func<Game, bool>> GetFilterExpression(MediaFilter mediaFilter, string? userId)
-		{
-			Expression<Func<Game, bool>> filter = g => true;
-
-			if (mediaFilter.SearchString != null)
-			{
-				filter = g => g.Name.Contains(mediaFilter.SearchString);
-			}
-
-			if (mediaFilter.MyMedia && userId != null)
-			{
-				filter = filter.And(g => g.MyGames != null && g.MyGames.Any(m => m.LuminaUserId == userId));
-			}
-
-			return filter;
+			var result = await _service.GetAndMapEntities<GamesDto>(mediaFilter, Includes, userId);
+			return result.Match<ActionResult<PaginatedResult<GamesDto>>>(
+				m => Ok(m),
+				f => BadRequest(f));
 		}
 	}
 }
