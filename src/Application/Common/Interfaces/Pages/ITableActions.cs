4967e4638930c7d40731399fc1fa0a7affa37c8f
xXAinzXx <dominic.tosku@gmail.com>
2024-02-11 11:39:53 +0100

    style: table grid view

---
+++ src/Application/Common/Interfaces/Pages/ITableActions.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+
+namespace Application.Common.Interfaces.Pages
+{
+    public interface ITableActions
+    {
+        string ToggleText { get; }
+
+        Task Delete(Game g);
+        void Dummy();
+        Task OnCreate();
+        Task OnDeleteChecked();
+        Task OnUpdate(Game g);
+        Task ReloadData();
+    }
+}
