ba640c07d1dee4bf4680b616657a96d53d8091c2
dominictosku <dominic.tosku@gmail.com>
2024-12-30 11:33:36 +0100

    feat: user service

--- src/LuminaPath/Components/BaseAuth/BaseAuth.cs
+++ src/LuminaPath/Components/BaseAuth/BaseAuth.cs
@@ -1,23 +1,29 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using LuminaPath.Core.Models;
+using LuminaPath.Infrastructure.Services;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace LuminaPath.Components.BaseAuth
 {
     public class BaseAuth : ComponentBase
     {
-        [Inject]
-        public AuthenticationStateProvider GetAuthenticationStateAsync { get; set; }
+        [CascadingParameter] private Task<AuthenticationState> AuthState { get; set; } = default!;
+
+        [Inject] public LuminaUserService LuminaUserService { get; set; }
+
+        public LuminaUser? User { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            var authstate = await GetAuthenticationStateAsync.GetAuthenticationStateAsync();
-            var user = authstate.User;
+            var state = await AuthState;
+            var user = state.User;
             if (!user.Identity.IsAuthenticated) return;
             var name = user.Identity.Name;
             UserName = name;
             UserId = user.FindFirst(c => c.Type.Contains("nameidentifier"))?.Value;
+            User = await LuminaUserService.GetUser(UserId);
         }
     }
 }
