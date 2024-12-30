d67cfb59cdc33a3090e49e3143be7a688b8af59d
dominictosku <dominic.tosku@gmail.com>
2024-12-30 14:52:06 +0100

    feat: add permissions

--- src/LuminaPath/Components/BaseAuth/BaseAuth.cs
+++ src/LuminaPath/Components/BaseAuth/BaseAuth.cs
@@ -2,6 +2,7 @@
 using LuminaPath.Infrastructure.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
 
 namespace LuminaPath.Components.BaseAuth
 {
@@ -9,17 +10,22 @@ namespace LuminaPath.Components.BaseAuth
     {
         [CascadingParameter] private Task<AuthenticationState> AuthState { get; set; } = default!;
 
-        [Inject] public LuminaUserService LuminaUserService { get; set; }
+        [Inject] private LuminaUserService LuminaUserService { get; set; } = default!;
+
+        private ClaimsPrincipal? claimsPrincipal { get; set; }
 
         public LuminaUser? User { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
+
+        public bool CanUserEdit => claimsPrincipal is null ? false : claimsPrincipal.IsInRole("Editor") || claimsPrincipal.IsInRole("Administrator");
 
         protected override async Task OnInitializedAsync()
         {
             var state = await AuthState;
             var user = state.User;
             if (!user.Identity.IsAuthenticated) return;
+            claimsPrincipal = user;
             var name = user.Identity.Name;
             UserName = name;
             UserId = user.FindFirst(c => c.Type.Contains("nameidentifier"))?.Value;
