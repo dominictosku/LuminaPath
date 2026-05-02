1c032e294342b291864445cfa04d73884bad7d5c
dominictosku <dominic.tosku@gmail.com>
2026-05-02 22:50:02 +0200

    feat: improve filters for grid

--- src/LuminaPath.Core/Entities/MediaFilter.cs
+++ src/LuminaPath.Core/Entities/MediaFilter.cs
@@ -1,11 +1,23 @@
-﻿namespace LuminaPath.Core.Entities
+using LuminaPath.Core.Enums;
+
+namespace LuminaPath.Core.Entities
 {
     public class MediaFilter
     {
         public string? SearchString { get; set; }
         public DateTime? From { get; set; }
         public DateTime? To { get; set; }
+        public Platforms? Platform { get; set; }
+        public GameStatus? Status { get; set; }
+        public string? Genre { get; set; }
+        public string? Source { get; set; }
         public string? Publisher { get; set; }
+        public int? MinPlaytime { get; set; }
+        public int? MaxPlaytime { get; set; }
+        public int? Priority { get; set; }
+        public short? MinRating { get; set; }
+        public double? MinPlayedHours { get; set; }
+        public bool OnlyWithRemainingHours { get; set; }
         public bool MyMedia { get; set; }
         public Paging Paging { get; set; } = new Paging();
     }
