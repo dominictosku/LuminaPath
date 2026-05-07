4175af4ce4804e4808b7e21e74205c964343587d
dominictosku <dominic.tosku@hotmail.com>
2026-05-07 18:00:14 +0200

    feat: add friends quick launch menu

--- src/LuminaPath.MobileApp/src/app/shared/components/navigation/nav-bar/nav-bar.component.ts
+++ src/LuminaPath.MobileApp/src/app/shared/components/navigation/nav-bar/nav-bar.component.ts
@@ -14,6 +14,7 @@ import {
   rocketOutline,
   settingsOutline,
   sparklesOutline,
+  peopleOutline,
   timeOutline,
 } from 'ionicons/icons';
 import { AuthService } from 'src/app/core/auth/services/auth.service';
@@ -56,6 +57,7 @@ export class NavBarComponent implements OnInit, OnDestroy {
       settingsOutline,
       sparklesOutline,
       timeOutline,
+      peopleOutline,
     });
   }
 
