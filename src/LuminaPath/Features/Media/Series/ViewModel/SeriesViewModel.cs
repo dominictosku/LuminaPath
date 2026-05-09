22b17511242c795259e4c6bdb22b904e3dc8d210
dominictosku <dominic.tosku@gmail.com>
2026-05-09 13:46:24 +0200

    feat: add series blazor pages

---
+++ src/LuminaPath/Features/Media/Series/ViewModel/SeriesViewModel.cs
@@ -0,0 +1,9 @@
+using LuminaPath.Core.Interfaces;
+using LuminaPath.Core.Models;
+
+namespace LuminaPath.Features.Media.Series.ViewModel
+{
+    public class SeriesViewModel : Core.Models.Series, IMedia<MediaDocument>
+    {
+    }
+}
